b694a859489c8e995532a65980793b2668ce3e13
Andrew <andrewsafwatsamuel@gmail.com>
2019-10-19 23:40:57 +0200

    created Region pojo

---
+++ app/src/main/java/com/aad/alc4/team10/animatedweatherapp/model/Region.kt
@@ -0,0 +1,26 @@
+import android.os.Parcel
+import android.os.Parcelable
+import com.aad.alc4.team10.animatedweatherapp.model.Country
+import com.squareup.moshi.Json
+
+data class Region(
+    @field:Json(name = "Name") val name: String?,
+    @field:Json(name = "Countries") val countries: List<Country>?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.createTypedArrayList(Country)
+    )
+
+    override fun writeToParcel(p0: Parcel, p1: Int) = with(p0) {
+        writeString(name)
+        writeTypedList(countries)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Region> {
+        override fun createFromParcel(parcel: Parcel)=Region(parcel)
+        override fun newArray(size: Int): Array<Region?> = arrayOfNulls(size)
+    }
+}
