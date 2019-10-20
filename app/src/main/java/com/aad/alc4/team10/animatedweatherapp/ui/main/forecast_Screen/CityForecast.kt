4116602208c9ca3e3924b9c492938aae56a5405e
Khalid Elnagar <khalid.3lnagar@gmail.com>
2019-10-20 21:45:17 +0200

    add: error handling with animation

--- app/src/main/java/com/aad/alc4/team10/animatedweatherapp/ui/main/forecast_Screen/CityForecast.kt
+++ app/src/main/java/com/aad/alc4/team10/animatedweatherapp/ui/main/forecast_Screen/CityForecast.kt
@@ -1,13 +1,17 @@
 package com.aad.alc4.team10.animatedweatherapp.ui.main.forecast_Screen
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.animation.doOnCancel
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.aad.alc4.team10.animatedweatherapp.R
+import com.aad.alc4.team10.animatedweatherapp.core.showAnimator
 import com.aad.alc4.team10.animatedweatherapp.ui.Forecast
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.city_forecast_fragment.*
@@ -35,6 +39,7 @@ class CityForecast : Fragment(), CityForecastAdapter.ForecastAdapterOnClickHandl
             mClickHandler = this
         )
     }
+    private val errorObjectAnimator by lazy { img_error.showAnimator() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,34 +61,69 @@ class CityForecast : Fragment(), CityForecastAdapter.ForecastAdapterOnClickHandl
 
     private fun initViewModel() = viewModel.run {
         loading.observe(this@CityForecast, Observer { onLoading(it) })
-        error.observe(this@CityForecast, Observer { onError(it) })
-        loadForecast(CAIRO_ID)
+        loadForecast(CAIRO_ID, ::onError)
 
     }
 
     private fun onLoading(loading: Boolean) {
-        rv_city_forecasts_list.visibility = if (loading) View.GONE else View.VISIBLE
         pb_loading.visibility = if (loading) View.VISIBLE else View.GONE
     }
 
-    private fun onError(error: Boolean?) {
-        if (error == true) {
-            //handel error
-            rv_city_forecasts_list.visibility = View.GONE
-            showSnackbar()
+    private fun onError(error: Boolean?) = if (error == true) {
+        //handel error
+        showErrorImage()
+        showSnackbar()
 
-        } else {
-            //return to normal state
-            rv_city_forecasts_list.visibility = View.VISIBLE
+    } else
+        hideErrorImage()
 
+    private fun showErrorImage(): Unit = with(img_error) {
+        scaleX = 0f
+        scaleY = 0f
+        visibility = View.VISIBLE
+        animate().apply {
+            cancel()
+            duration = 500
+            scaleX(1f)
+            scaleY(1f)
+            setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    errorObjectAnimator.start()
+                }
+            }
+            )
+            start()
         }
 
+
+    }
+
+    private fun hideErrorImage(): Unit = with(img_error) {
+        scaleX = 1f
+        scaleY = 1f
+        animate().apply {
+            cancel()
+            duration = 500
+            scaleX(0f)
+            scaleY(0f)
+            setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    errorObjectAnimator.doOnCancel { visibility = View.GONE }
+                    errorObjectAnimator.cancel()
+
+                }
+            })
+            start()
+        }
     }
 
     private fun showSnackbar() {
         Snackbar
             .make(rv_city_forecasts_list, getString(R.string.error), Snackbar.LENGTH_INDEFINITE)
-            .setAction(getString(R.string.try_again)) { viewModel.loadForecast(CAIRO_ID) }
+            .setAction(getString(R.string.try_again)) {
+                onError(false)
+                viewModel.loadForecast(CAIRO_ID, ::onError)
+            }
             .show()
     }
 
