d96f65a288db79b46a200a160c89d36ddf946ae9
Andrew <andrewsafwatsamuel@gmail.com>
2019-10-20 13:43:33 +0200

    fixed card colors & added ripple effect

--- app/src/main/java/com/aad/alc4/team10/animatedweatherapp/ui/main/region_screen/MyRegionRecyclerViewAdapter.kt
+++ app/src/main/java/com/aad/alc4/team10/animatedweatherapp/ui/main/region_screen/MyRegionRecyclerViewAdapter.kt
@@ -1,25 +1,34 @@
 package com.aad.alc4.team10.animatedweatherapp.ui.main.region_screen
 
 import Region
-import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.palette.graphics.Palette
 import com.aad.alc4.team10.animatedweatherapp.R
+
+fun createPaletteSync(bitmap: Bitmap): Palette = Palette.from(bitmap).generate()
 
 class RegionViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
     private val photo by lazy { mView.findViewById<ImageView>(R.id.region_photo_image_view) }
     private val nameTextView by lazy { mView.findViewById<TextView>(R.id.region_name_text_view) }
 
+    private val card by lazy { mView.findViewById<CardView>(R.id.item_region_card_view) }
+
     fun bind(region: Region) = region
         .also { photo.setImageResource(setRegionPhoto(it)) }
-        .run { nameTextView.text = name }
+        .apply { nameTextView.text = name }
+        .let { createPaletteSync(mView.context.resources.getDrawable(setRegionPhoto(it)).toBitmap()) }
+        .run { card.setCardBackgroundColor(getDominantColor(mView.resources.getColor(R.color.off_white))) }
 
     private fun setRegionPhoto(region: Region): Int = when (region.name) {
         "MENA" -> R.drawable.mena
